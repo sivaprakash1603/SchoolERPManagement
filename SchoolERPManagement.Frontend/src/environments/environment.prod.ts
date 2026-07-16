14bc96a6e827db9a652ada76888fd81b5f3e7e51
Sivaprakash S <siva8012226560@gmail.com>
2026-07-16 16:11:45 +0530

    Automated update of backend IP to HTTPS Domain schoolerp-api-4ftuujzdmtwe6.centralindia.cloudapp.azure.com

--- SchoolERPManagement.Frontend/src/environments/environment.prod.ts
+++ SchoolERPManagement.Frontend/src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
   apiUrl: 'https://schoolerp-api-4ftuujzdmtwe6.centralindia.cloudapp.azure.com/api',
   baseUrl: 'https://schoolerp-api-4ftuujzdmtwe6.centralindia.cloudapp.azure.com',
   hubUrl: 'https://schoolerp-api-4ftuujzdmtwe6.centralindia.cloudapp.azure.com/hubs',
-  aiApiUrl: 'http://localhost:8000'
+  aiApiUrl: 'https://schoolerp-api-4ftuujzdmtwe6.centralindia.cloudapp.azure.com'
 };
